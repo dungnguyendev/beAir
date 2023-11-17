90a5a41677566efb4dbf26b6c04bc3e265ed30dc
dungnguyendev <dungnguyentien.dev@gmail.com>
2023-11-17 22:46:15 +0700

    3001

--- app.js
+++ app.js
@@ -16,11 +16,11 @@
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
 
-  // app.use(function (req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //   next();
-  // });
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(logger('dev'));
